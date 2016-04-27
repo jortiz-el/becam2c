299b21bf9cbbfb903d44520ab83e3d7bfc4ddd90
Joao Ortiz Alegre <jortiz@m2c.2k3>
2016-04-27 17:38:43 +0200

    alerta javascrit para ejecutar desde funcion estatica en C#

--- ASP.NET/scriptAlertaCodebehind.cs
+++ ASP.NET/scriptAlertaCodebehind.cs
@@ -1,8 +1,11 @@
-
 //funcion estatica para llamar desde cualquier parte en el proyecto
-
 public static void resgistrarError(string msjExcepcion, Page page)
 {
     string script = @" alert('" + msjExcepcion.Replace("'", "").Replace("\n", "") + "');";
     ScriptManager.RegisterStartupScript(page, page.GetType(), "alerta", script, true);
 }
+
+//alerta javascript desde code behind
+string script = @" alert('Cliente insertado correctamente ');";
+ScriptManager.RegisterStartupScript(this, GetType(), "alerta", script, true);
+limpiarFormulario();
